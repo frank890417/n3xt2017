bab2e32d6f1022f82100ca09c9da261399d32ed8
cheyuwu <frank890417@gmail.com>
2017-10-23 09:54:42 +0800

    update images

--- resources/assets/js/pages/page_index.vue
+++ resources/assets/js/pages/page_index.vue
@@ -31,6 +31,7 @@
             span Connecting
             br
             span Infinity
+          img(src="/img/s2_people.png")
         .col-sm-6
           h2 n3xtcon is a haven for creative authenticity.
           hr
@@ -45,6 +46,9 @@
           p Our team consists of individuals from creative, business, technology, and science backgrounds, and it is our dream to expand our community with such diversity across the world.<br><br>On the one hand, we promote scalability while innovating on a global scale, but on the other hand, we want to emphasize the unlimited potential for any innovative problem-solving ideas in daily life.
           .btn.red About us
         .col-sm-6
+          .block.blue
+          img(src="/img/s2_people.png")
+
   section.sectionPlatform.blue
     .container
       .row
@@ -75,6 +79,7 @@
           p Starting any journey with the end in mind makes perfect sense. However when it comes to career kickstarts, changes and promotions, it’s important to think about the embarkation point and plan from there. 
           .btn.red RSVP Now
         .col-sm-6 
+          .block.blue
       
   section.sectionEvent.blue
     .container
@@ -82,19 +87,21 @@
         .col-sm-6 
           h2 Annual Timeline
           hr
-          h4 9/7/2017   THU.  6PM /  UCS EEB 132 
-          .event
-            h3 9/7/2017   THU.  6PM
+          .event.purple
+            h4 9/7/2017   THU.  6PM
             h2 WORKSHOP
             p Starting any journey with the end in mind makes perfect sense. However when it comes to career kickstarts.. 
           .btn.ghost.white RSVP Now
         .col-sm-6 
-      
-  section.sectionPartner.white
+  section.sectionPartner.white.text-center
     .container
       .row
         .col-sm-12
           h2 STRATEGIC PARTNERS
+        .col-sm-12
+          img.logo(src="http://www.crosscamp.us/wp-content/uploads/2015/03/CrossCampus.png")
+          img.logo(src="https://liaoyuanavatar.imgix.net/upload/feed/image/159911bb496-57ea9af32cb6a8ad7e63e2c1-a1412a8bb2bd8775?fit=max&w=960&h=600")
+          img.logo(src="http://media.marketwire.com/attachments/201005/598031_Logo_300dpi_noline_nochinese.jpg")
 
   section.sectionContact.blue
     .container
@@ -104,15 +111,16 @@
           hr
           p Feel free to contact us for any questions,  recommendations, speaking engagements, and inquiries about sponsorship and partnership.
           .infos
-            .email Email
-            .call Call
+            .email Email |  info@n3xtcon.com
+
+            .call Call ​| (213) 536-1923
           
         .col-sm-6
           .form-group
             input.form-control
             input.form-control
             textarea.form-control
-
+            .btn.red Send
   section.sectionCopyright.blue
     .container
       .row

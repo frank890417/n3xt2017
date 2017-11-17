020e4c6f068237fd5a0919088a18ce4ad4ea406e
cheyuwu <frank890417@gmail.com>
2017-11-17 11:44:54 +0800

    update about color schema and structure

--- resources/assets/js/pages/page_about.vue
+++ resources/assets/js/pages/page_about.vue
@@ -6,7 +6,124 @@
     .container
       .row
         .col-sm-12
-          h1 About
+          h1 About us
+          h3 SOCAL'S FAST GROWING TECH COMMUNITY WITH LA SPIRIT
+          hr
+          h5 
+  section.sectionWhat.white
+    .container
+      .row
+        .col-sm-6
+          h2 What is n3xt con?
+          hr
+          p n3xt con is a haven for creative and intellectual authenticity. We believe entrepreneurial spirit is an immutable individual trait that cannot be smothered. n3xt con’s mission is to establish an environment in which such spirit not only flourishes but inspires more people to make a long lasting impact in their lives and beyond.<br> <br>As globalization becomes the foundation to our future, it is important for tomorrow’s thought leaders to establish an apolitical vision today. n3xt con sees the future through an entrepreneurial lens. For us, a “startup” merely means a new way of doing things so there is no start-up that is too small or insignificant. On the one hand, we promote scalability while innovating on a global scale, but on the other hand, we want to emphasize the unlimited potential for any innovative problem-solving ideas in daily life.<br><br>Our team consists of individuals from creative, business, technology, and science backgrounds, and it is our dream to expand our community with such diversity across the world.
+        
+  section.sectionDo.blue
+    .container
+      .row
+        .col-sm-6
+          img(src="https://lh3.googleusercontent.com/J97buJ0AuD6ZdTxg7EDG9k6pPkbFuXvuDpLb0ITWpcUCspIPrHkx5l2YNSFR_iwebAF23wP8-0acxTh-BK973FetsaBZ5F5UJmcZoL1Fg8WMsAZRmo0MFAOMNvvAUoSTXG2pWqmmnT0u9gz2Q9Zjy10b3muiTFNPYDaaLBFdaAsoBgolk6p6bZFpDAl31amXEvmq_rhV-2OfYnZKMBrsCQRqnmotuS7j95ipyBRHStYIBB0QJYMLpTo3n2Uzf31mkczMqj5GmywOAHgpQ-aVOVVfIyCLPpoIhb_5hchoYVNXM4GXYnBja8_bGUWVMX8479nIbORjIJZGtBBNFc8hCJffToH73EOwuhJ127nJHXYaTKfFbFCeL_d-GGo53DOZzaakk8ZXRl8-7sQJwpCQnjjUaqt1XZ44H5IDwBDBGOkbmcO4gF2M2ydRAeOVjgk2NmifrEXPK8v_G7m9RK-_Mm3jaGqRbmISa7UsZkFiLakmY9XaklFmFezF2UnFOhnpOoaCFGgzA9YVP4RApZTqoShT8Xxft_VIk8CwM-Queg5aewLxocP1PYCzGM1GVujRzApdchOTA7IdMcTKLI5PppxEb5LG-Wtfydl6tyuHW593SC5JuBx8D-DQK6g2SsE2qp_WLbbe_Pr-yY-8Y5ANYN0BvOdKT-wbjE8=w1442-h961-no" style="width: 100%")
+          
+        .col-sm-6
+          h3.titleSmall About us
+          h2 What we do?
+          hr
+          p ​Each year, we host a variety of events including workshops, mentorship programs, networking events, mixers, and our annual national conference. In 2016 & 2017, we hosted one of the largest annual conferences in Southern California which attracted over 700 working professionals, startup founders, investors, and industry leaders
+
+  section.sectionMission.white
+    .container
+      .row
+        .col-sm-7
+          h2 Mission & Vision statement
+          hr
+          p ​Each year, we host a variety of events including workshops, mentorship programs, networking events, mixers, and our annual national conference. In 2016 & 2017, we hosted one of the largest annual conferences in Southern California which attracted over 700 working professionals, startup founders, investors, and industry leaders
+        .col-sm-5
+          h1  hand pic(red)
+
+
+  section.sectionDo.blue
+    .container
+      .row
+        .col-sm-6
+          h3.titleSmall People working with us
+          h2 Team & Culture
+          hr
+          p ​Each year, we host a variety of events including workshops, mentorship programs, networking events, mixers, and our annual national conference. In 2016 & 2017, we hosted one of the largest annual conferences in Southern California which attracted over 700 working professionals, startup founders, investors, and industry leaders
+
+        .col-sm-6
+          h1 img here
+
+  
+  section.sectionOpening.blue
+    .container
+      .row
+        .col-sm-12
+          h3.titleSmall Join us!
+          h2 Current Opening
+          hr
+
+      .row
+        .col-sm-4
+          ul.positionList
+            li
+              h3 Position Name
+              h4 A Sentence Describe about
+            li
+              h3 Position Name
+              h4 A Sentence Describe about
+            li
+              h3 Position Name
+              h4 A Sentence Describe about
+        .col-sm-8
+          .panel.white
+            p ​Each year, we host a variety of events including workshops, mentorship programs, networking events, mixers, and our annual national conference. In 2016 & 2017, we hosted one of the largest annual conferences in Southern California which attracted over 700 working professionals, startup founders, investors, and industry leaders
+            .btn.red Apply
+
+  section.sectionSponsor.blue
+    .container
+      .row
+        .col-sm-12
+          h3.titleSmall Join us!
+          h2 Sponsorship & Donation 
+          hr
+
+      .row
+        .col-sm-3
+          .card
+            img
+            h3 A cup of coffee
+            p Label :    some requirement​<br>Each year, we host a variety of events including workshops, 
+            ul
+              li Content 1 of something
+              li Content 2 of blabal
+            .btn.red Sponsor
+        .col-sm-3
+          .card
+            img
+            h3 Small Donation
+            p Label :    some requirement​<br>Each year, we host a variety of events including workshops, 
+            ul
+              li Content 1 of something
+              li Content 2 of blabal
+            .btn.red Sponsor
+        .col-sm-3
+          .card
+            img
+            h3 Meduim Donation
+            p Label :    some requirement​<br>Each year, we host a variety of events including workshops, 
+            ul
+              li Content 1 of something
+              li Content 2 of blabal
+            .btn.red Sponsor
+        .col-sm-3
+          .card
+            img
+            h3 Company Donation
+            p Label :    some requirement​<br>Each year, we host a variety of events including workshops, 
+            ul
+              li Content 1 of something
+              li Content 2 of blabal
+            .btn.red Sponsor
 
 </template>
 

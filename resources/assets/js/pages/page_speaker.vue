f5d86870c9c08c16fd4c39c6b9dad9f3fa1a4f18
cheyuwu <frank890417@gmail.com>
2017-11-23 12:51:02 +0800

    update speaker page version 1

--- resources/assets/js/pages/page_speaker.vue
+++ resources/assets/js/pages/page_speaker.vue
@@ -6,7 +6,87 @@
     .container
       .row
         .col-sm-12
-          h2 Speaker
+          h1 Speaker
+          h3 SOCAL'S FAST GROWING TECH COMMUNITY WITH LA SPIRIT
+          hr
+          h5 
+
+        img.logo.line(src="/img/logoSymbolLine.svg")
+        //img.logo(src="/img/logoSymbolShadow.svg",:style="{transform: `translate(0,${parseInt(scrollTop/8)}px`}")
+        
+  section.sectionDescription.white
+    .container
+      .row
+        .col-sm-11.colContent
+          .row
+            .col-sm-9
+              h2 Title for Speakers of recent events 
+              hr
+              p ​Each year, we host a variety of events including workshops, mentorship programs, networking events, mixers, and our annual national conference. In 2016 & 2017, we hosted one of the largest annual conferences in Southern California which attracted over 
+            .col-sm-3
+              h1 200+<br>Speakers
+  section.sectionSpeakers
+    .container-fluid
+      .row
+        .col-sm-3
+          .person(v-for="i in 2")
+            h3 Speaker name
+            h4 Company Name or position
+
+        .col-sm-9
+          .person.big
+          .infos.red
+            h2 Some feature<br>speakers title<br>description
+            p ​Each year, we host a variety of events including workshops, mentorship programs, networking events, mixers, and our annual national conference. In 
+            .btn.white Relevant Events
+      .row
+
+        .col-sm-9
+          .infos
+            h2 Some feature<br>speakers title<br>description
+            p ​Each year, we host a variety of events including workshops, mentorship programs, networking events, mixers, and our annual national conference. In 
+      
+          .person.big
+
+        .col-sm-3
+          .person
+          .person
+
+
+  section.sectionSmallDescription.white
+    .container
+      .row
+        .col-sm-7.colContent
+          h2 Title for past event<br>speakers who which<br>has smaller display area
+          hr
+          p ​Each year, we host a variety of events including workshops, mentorship programs, networking events, mixers, and our annual national conference. In 2016 & 2017, we hosted one of the largest annual conferences in Southern California which attracted over 700 working professionals, startup founders, investors, and industry leaders
+        .col-sm-5
+          img.logo(src="/img/logoSymbolShadow.svg",:style="{transform: `translate(0,${parseInt(scrollTop/8)}px`}")
+     
+  section.sectionSpeakers.white   
+    .row
+      .col-sm-3(v-for="i in 4")
+        .person
+          h3 Speaker name
+          h4 Company Name or position
+    .row
+      .col-sm-3(v-for="i in 2")
+        .person
+          h3 Speaker name
+          h4 Company Name or position
+    
+      .col-sm-6
+        .redBlock
+          .hand
+          h2 Some Title<br>or Slogan<br>about n3xt or spirit
+    .row
+      .col-sm-3(v-for="i in 4")
+        .person
+          h3 Speaker name
+          h4 Company Name or position
+    
+
+
   section_footer
 </template>
 

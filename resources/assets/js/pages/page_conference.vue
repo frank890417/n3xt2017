531c372f9aff64c70896b202717792aa5a739d18
frank890417 <frank890417@gmail.com>
2019-02-10 20:06:55 -0500

    update conference apge to dynamic structure

--- resources/assets/js/pages/page_conference.vue
+++ resources/assets/js/pages/page_conference.vue
@@ -7,9 +7,11 @@
           img(src="/img/theme1.svg")
         .col-sm-5.col-info
           h4 {{ getDurationText( event.start_datetime,event.end_datetime ) }}
-          h1 n3xt con 2018
+          h1 {{ contentData.title }}
           h3 ​
-            span Haven for Entrepreneurs and Storytellers<br><br>
+            span {{ contentData.subtitle }}
+            br
+            br
             hr
             span
               i.fa.fa-map-marker  
@@ -17,36 +19,17 @@
           .btn(@click="scrollTo('.sectionRegistration');$ga.event('conference','get tickets','click')") Get Tickets
   section.sectionAsk.white
     .container
-      .row
+      .row(v-for="(section,sid) in contentData.abouts")
+        .col-sm-6.wow.fadeIn(v-if="sid % 2!=0")
+          img(:src="section.img")
         .col-sm-6
-          h2.wow.fadeIn n3xt con 2018
+          h2.wow.fadeIn(v-html="section.title")
           hr
-          p.wow.fadeIn N3xt con 2018 brings together bright minds and visionaries to share ideas of innovation. Featuring a variety of themes including 
-            b Humancentric Technology
-            span , 
-            b Omnipresence of Storytelling
-            span , 
-            b Frontier of Imagination
-            span . Talk topics range from blockchain, brain-computer interface, AI, interactive media, to film production. <br><br>The conference provides an opportunity for attendees to get fresh perspective from industry leaders and futurists. Come, listen, get inspired and establish the network that may help you forge the path you have yet to discover.
-
-        .col-sm-6.wow.fadeIn
-          img(src="/img/logohexi.svg")
-      .row
-        .col-sm-6.wow.fadeIn
-          img.leftout(src="/img/conf_speaker.png")
-        .col-sm-6
-          h2.wow.fadeIn Speakers
-          hr
-          p.wow.fadeIn High-quality programs and inspirational speakers. This year, we selected the best and brightest group of entrepreneurs, business investors, pioneers in technology, and industry leaders in the fields of technology, media, and entertainment.
-          .btn.blue(@click="scrollTo('.sectionSpeakers')") Speaker Lineup
-      .row
-        .col-sm-6
-          h2.wow.fadeIn Attendees
-          hr
-          p.wow.fadeIn If you're a visionary, then this is for you. N3xt con 2018 attendees are comprised of hundreds of professionals and visionaries who come together to inspire each other and exchange knowledge. Attendees range from Fortune 500 companies to smaller tech companies.
-          .btn.blue(@click="scrollTo('.sectionRegistration')")  Get Ticket
-        .col-sm-6.wow.fadeIn
-          img(src="/img/cf01.jpg")
+          p.wow.fadeIn(v-html="section.content")
+          .btn.blue(v-if="section.btn",
+                    @click="scrollTo(section.btn.target)") {{ section.btn.text }}
+        .col-sm-6.wow.fadeIn(v-if="sid % 2==0")
+          img(:src="section.img")
   section.sectionHighlight.blue
     .container
       .row
@@ -54,18 +37,10 @@
           h2 Program Highlight
           hr
       .row
-        .col-sm-4
-          img(src="/img/theme1.svg").wow.zoomIn
-          h3.wow.fadeIn Humancentric Technology 
-          p.wow.fadeIn Showcase innovations that bring form and purpose to our endless imaginations. A lineup of visionaries share the inner-workings of their explorations in engineering that enhance the human experience and connection in a global landscape.
-        .col-sm-4
-          img(src="/img/theme2.svg").wow.zoomIn
-          h3.wow.fadeIn Omnipresence of Storytelling
-          p.wow.fadeIn Storytelling allows us to establish trust and community, breaks down barriers, and bridges our souls with empathy. With technology, storytelling has taken new forms and deepened its cultural impact like never before. Creative movers and shapers in the heart of global entertainment unveil groundbreaking ways of continuing this ancient tradition in branding, film & television, and marketing.
-        .col-sm-4
-          img(src="/img/theme3.svg").wow.zoomIn
-          h3.wow.fadeIn Frontier of Imagination
-          p.wow.fadeIn Modern entrepreneurs are capturing imaginations by colonizing space, revolutionizing energy sources, and enhancing global interactions. Often times, the possible is built on ideas that once considered impossible. New generations of entrepreneurs discuss how they apply their imagination to disrupting ideas that fundamentally change us.
+        .col-sm-4(v-for="highlight in contentData.highlights")
+          img(:src="highlight.img").wow.zoomIn
+          h3.wow.fadeIn(v-html="highlight.title")
+          p.wow.fadeIn(v-html="highlight.content")
   section.sectionKeynote.white
     .container
       .row
@@ -221,43 +196,92 @@ export default {
       ],
       id: 9,
       // routename: "n3xtconf_2018",
-      keynotes: [
-        {
-          theme: "Frontier of Imagination",
-          title: "Unlocking the Mysteries of Our Brain",
-          company: "Founder & CEO of Openwater, Former Executive of Facebook Oculus",
-          speaker: "Mary Lou Jepsen",
-          speakerId: 123,
-          cover: "/img/imag001.jpg"
-        },
+      contentData: {
+        title: "n3xt con 2018",
+        subtitle: "Haven for Entrepreneurs and Storytellers",
+        abouts: [
+          {
+            title: "n3xt con 2018",
+            content: `N3xt con 2018 brings together bright minds and visionaries to share ideas of innovation. Featuring a variety of themes including <b>Humancentric Technology</b><span>, </span><b>Omnipresence of Storytelling</b><span>, </span><b>Frontier of Imagination</b><span>. Talk topics range from blockchain, brain-computer interface, AI, interactive media, to film production. <br><br>The conference provides an opportunity for attendees to get fresh perspective from industry leaders and futurists. Come, listen, get inspired and establish the network that may help you forge the path you have yet to discover.</span>`,
+            img: "/img/logohexi.svg"
+          },
+          {
+            title: "Speakers",
+            content: "High-quality programs and inspirational speakers. This year, we selected the best and brightest group of entrepreneurs, business investors, pioneers in technology, and industry leaders in the fields of technology, media, and entertainment.",
+            img: "/img/conf_speaker.png",
+            btn: {
+              target: ".sectionSpeakers",
+              text: "Speaker Lineup"
+            }
+          },
+          {
+            title: "Attendees",
+            content: "If you're a visionary, then this is for you. N3xt con 2018 attendees are comprised of hundreds of professionals and visionaries who come together to inspire each other and exchange knowledge. Attendees range from Fortune 500 companies to smaller tech companies.",
+            img: "/img/cf01.jpg",
+            btn: {
+              target: ".sectionRegistration",
+              text: "Get Ticket"
+            }
+          }
+        ],
+        highlights: [
+          {
+            title: "Humancentric Technology",
+            img: "/img/theme1.svg",
+            content: "Showcase innovations that bring form and purpose to our endless imaginations. A lineup of visionaries share the inner-workings of their explorations in engineering that enhance the human experience and connection in a global landscape."
+          },{
+            title: "Omnipresence of Storytelling",
+            img: "/img/theme2.svg",
+            content: "Storytelling allows us to establish trust and community, breaks down barriers, and bridges our souls with empathy. With technology, storytelling has taken new forms and deepened its cultural impact like never before. Creative movers and shapers in the heart of global entertainment unveil groundbreaking ways of continuing this ancient tradition in branding, film & television, and marketing."
+          },{
+            title: "Frontier of Imagination",
+            img: "/img/theme3.svg",
+            content: "Modern entrepreneurs are capturing imaginations by colonizing space, revolutionizing energy sources, and enhancing global interactions. Often times, the possible is built on ideas that once considered impossible. New generations of entrepreneurs discuss how they apply their imagination to disrupting ideas that fundamentally change us."
+          }
+        ],
+        keynotes: [
+          {
+            theme: "Frontier of Imagination",
+            title: "Unlocking the Mysteries of Our Brain",
+            company: "Founder & CEO of Openwater, Former Executive of Facebook Oculus",
+            speaker: "Mary Lou Jepsen",
+            speakerId: 123,
+            cover: "/img/imag001.jpg"
+          },
 
-        {
-          theme: "Omnipresence of Storytelling",
-          title: "Kick-Ass Asian Female Filmmakers",
-          company: "Award Winning Film Director",
-          speaker: "Livi Zheng",
-          speakerId: 127,
-          cover: "/img/imag002.jpg"
-        },
+          {
+            theme: "Omnipresence of Storytelling",
+            title: "Kick-Ass Asian Female Filmmakers",
+            company: "Award Winning Film Director",
+            speaker: "Livi Zheng",
+            speakerId: 127,
+            cover: "/img/imag002.jpg"
+          },
 
-        {
-          theme: "Omnipresence of Storytelling",
-          title: "Future Home Entertainment - Vessel for Storytelling",
-          company: "Founder & CEO",
-          speaker: "William Wang",
-          speakerId: 108,
-          cover: "/img/imag003.jpg"
-        },
+          {
+            theme: "Omnipresence of Storytelling",
+            title: "Future Home Entertainment - Vessel for Storytelling",
+            company: "Founder & CEO",
+            speaker: "William Wang",
+            speakerId: 108,
+            cover: "/img/imag003.jpg"
+          },
 
-        {
-          theme: "Omnipresence of Storytelling",
-          title: "Kick-Ass Asian Female Filmmakers",
-          company: "Award Winning Film Director",
-          speaker: "Roxy Shih",
-          speakerId: 129,
-          cover: "/img/imag004.jpg"
-        },
-      ],
+          {
+            theme: "Omnipresence of Storytelling",
+            title: "Kick-Ass Asian Female Filmmakers",
+            company: "Award Winning Film Director",
+            speaker: "Roxy Shih",
+            speakerId: 129,
+            cover: "/img/imag004.jpg"
+          },
+        ],
+        registration: {
+          title: "Registration",
+          content: "N3xt con 2018 is scheduled for June 16-17 2018 at the Regent Theater DTLA. Limited Tickets Available! Feel free to contact us for any questions, recommendations, speaking engagements, and inquiries about sponsorship and partnership."
+        }
+
+      },
       slickOptions: {
           slidesToShow: 1,
           arrows: false,
@@ -302,11 +326,18 @@ export default {
       res.data.speaker = JSON.parse(res.data.speaker || "[]")
       res.data.album = JSON.parse(res.data.album || "[]")
       res.data.agencies = JSON.parse(res.data.agencies || "[]")
+      
+
+
 
       this.event=res.data
       this.event.program.forEach(p=>{
         p.speakers = JSON.parse(p.speakers)
       })
+
+      //merge content data
+      res.data.otherinfo = JSON.parse(res.data.otherinfo || "{}")
+      this.$set(this,"contentData",{...this.contentData,...res.data.otherinfo})
 
       if (this.event.agencies.length && typeof this.event.agencies[0]!='object'){
         this.event.agencies = this.event.agencies.map(id=>({id,type: 'organizer'}))
@@ -327,6 +358,9 @@ export default {
 
         },300)
       })
+
+
+
     })
     //if custom route exist then get by name, else get by id
     new WOW().init();
@@ -356,11 +390,11 @@ export default {
     },
     keynoteSlideDelta(value){
       let n = this.currentSlideId+value
-      if (n>this.keynotes.length-1){
+      if (n>this.slides.length-1){
         n=0
       }
       if (n<0){
-        n=this.keynotes.length-1
+        n=this.slides.length-1
       }
       this.currentSlideId=n
     },
@@ -390,7 +424,7 @@ export default {
       return this.speakers.find(sp=>sp.id==this.speakerShowId)
     },
     slides(){
-      return this.keynotes
+      return this.contentData.keynotes
     },
     currentSlide(){
       return this.slides[this.currentSlideId]
@@ -399,6 +433,7 @@ export default {
     navEvent(){
       let currentIndex = -1
       if (this.events){
+
         this.events.forEach((d,i)=>{
           console.log(d.id,this.event.id  )
           if (d.id==this.event.id){

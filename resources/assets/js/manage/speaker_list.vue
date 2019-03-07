ee3bcac8ee4d1a3a86b4720787f60a205bf581a6
frank890417 <frank890417@gmail.com>
2019-03-06 23:41:13 -0500

    fix speaker id mismatch and add search function for speaker by id

--- resources/assets/js/manage/speaker_list.vue
+++ resources/assets/js/manage/speaker_list.vue
@@ -10,24 +10,27 @@
         .panel.panel-primary
           .panel-heading Speaker List Manage
           .panel-body
-            router-link.btn.btn-primary(to="/manage/speaker/new") + Add New Speaker
-            input.forma-control(
-              v-model="keyword", 
-              placeholder="enter keyword...",
-              list="speakerlist")
+            .container-fluid
+              .row
+                .col-sm-10
+                  el-input.forma-control(
+                    v-model="keyword", 
+                    placeholder="enter keyword...",
+                    list="speakerlist")
+                .col-sm-2
+                  router-link.btn.btn-primary(to="/manage/speaker/new") + Add New Speaker
             datalist#speakerlist
               option(:value="speaker.name" v-for="speaker in sortedSpeaker")
             br
-            ul.list-group
-              .row
-                .col-sm-12
-                  
-                .col-sm-4(v-for = "(speaker,spid) in filteredSpeaker", style="height: 80px")
+            br
+            ul.list-group.mt-3
+              .row     
+                .col-sm-4.mt-2(v-for = "(speaker,spid) in filteredSpeaker", style="height: 80px")
                   .row
                     .col-sm-3
                       img(:src="speaker.headshot", style="width: 100%")
                     .col-sm-9
-                      h4 {{spid+1}} | {{speaker.name}}
+                      h4 {{speaker.id}} | {{speaker.name}}
                         router-link.btn.btn-primary.pull-right(:to="'/manage/speaker/'+speaker.id") Edit
                 
 </template>
@@ -69,7 +72,10 @@ export default {
         } )
     },
     filteredSpeaker(){
-      return this.sortedSpeaker.filter(speaker=>speaker.name.toLowerCase().indexOf(this.keyword.toLowerCase())!=-1)
+      return this.sortedSpeaker.filter(speaker=>
+        speaker.name.toLowerCase().indexOf(this.keyword.toLowerCase())!=-1 ||
+        speaker.id==this.keyword
+      )
        
     }
   }
